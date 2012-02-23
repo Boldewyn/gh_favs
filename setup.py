6a3aad9f1ee873553d513ec3f354ea4a90b8eb7a
Manuel Strehl <git@manuel-strehl.de>
2012-02-23 22:19:34 +0100

    fix issue where wrong clone URL was used

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info < (2, 6):
 
 setup(
     name = 'gh_favs',
-    version = '3.0',
+    version = '3.1',
     py_modules = ['gh_favs'],
     author = 'Manuel Strehl',
     author_email = 'boldewyn [at] googlemail.com',
